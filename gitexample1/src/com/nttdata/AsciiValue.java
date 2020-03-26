6118da15ff9ef18b3b7569895115e0efdfee744c
vrindaananda <nandaakku@gmail.com>
2020-03-26 14:58:20 +0530

    ascii value

---
+++ gitexample1/src/com/nttdata/AsciiValue.java
@@ -0,0 +1,15 @@
+package com.nttdata;
+
+public class AsciiValue {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
